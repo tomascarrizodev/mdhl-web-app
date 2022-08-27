664be57a3ebc3013c299d238730891896359dd55
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-08-26 21:50:50 -0300

    menú color

--- js/main.js
+++ js/main.js
@@ -3,17 +3,101 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        $header: '',
-        $menu: 'a',
+        header: `
+        <header id="header" class="d-flex justify-content-between align-items-center">
+        <img src="../assets/img/logo.png" alt="logo" id="logo">
+        <img src="../assets/img/title_1.png" alt="title" id="title">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+        </svg>
+        </header>`,
+        menu: `<nav id="nav" class="d-flex justify-content-around align-items-center">
+        <a href="#" id="home" v-if="document.title === 'Home'" class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+            </svg>
+            <span>Home</span>
+        </a>
+        <a href="../index.html" id="home" v-else class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+            </svg>
+            <span>Home</span>
+        </a>
+        <a href="#" id="about" v-if="document.title === 'About'" class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+            </svg>
+            <span>About</span>
+        </a>
+        <a href="../html/about.html" v-else id="about" class="icon d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+            </svg>
+            <span>About</span>
+        </a>
+        <a href="#" v-if="document.title === 'Games'" id="games" class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <img src="../assets/img/hockey_icon.png" width="24" height="24" alt="">
+            <span>Games</span>
+        </a>
+        <a href="./html/game_info.html" v-else id="games" class="icon d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <img src="../assets/img/hockey_icon.png" width="24" height="24" alt="">
+            <span>Games</span>
+        </a>
+        <a href="#" v-if="document.title === 'Rules'" id="rules" class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/>
+            </svg>
+            <span>Rules</span>
+        </a>
+        <a href="../html/rules.html" v-else id="rules" class="icon d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/>
+            </svg>
+            <span>Rules</span>
+        </a>
+        <a href="#" v-if="document.title === 'Contact'" id="contact" class="icon page d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+            </svg>
+            <span>Contact</span>
+        </a>
+        <a href="../html/contact.html" id="contact" class="icon d-flex flex-column justify-content-around align-items-center text-decoration-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+            </svg>
+            <span>Contact</span>
+        </a>
+    </nav>`,
+    title: `${document.title}`,
+    home: `${document.querySelector('#main')}`
       }
     },
     created() {
-      this.$header = document.querySelector('#header')
+      switch (this.title) {
+        case 'Home':
+          console.log('hola');
+          console.log(this.home)
+          console.log(document.getElementById('about'))
+          console.log(document.getElementById('game'))
+          console.log(document.getElementById('rules'))
+          console.log(document.getElementById('contact'))
+          break;
+      }
     },
     methods: {
-
+      show() {
+        document.querySelector('#open').classList.toggle('d-none')
+        document.querySelector('#close').classList.toggle('d-none')
+        document.querySelector('#aside').classList.toggle('hide')
+        document.querySelector('#aside').classList.toggle('show')
+        document.querySelector('#nav').classList.toggle('hide_nav')
+      },
+      
     },
     computed: {
-
+      
     }
   }).mount('#app')
