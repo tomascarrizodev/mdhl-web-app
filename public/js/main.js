6448d6ed0036b46e7facf2a2f2eeb4c164950a85
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-08-31 13:33:05 -0300

    nuevas y mejores disposiciones de menú y contenido

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,11 @@ createApp({
         show() {
             document.querySelector('#open').classList.toggle('d-none')
             document.querySelector('#close').classList.toggle('d-none')
-            document.querySelector('#aside').classList.toggle('hide')
+            // document.querySelector('#aside').classList.toggle('hide')
             document.querySelector('#aside').classList.toggle('show')
+            // document.querySelector('#aside').classList.toggle('d-none')
             document.querySelector('#nav').classList.toggle('hide_nav')
+            document.querySelector('#principal').classList.toggle('d-none')
         },
         toggleClass: function(element) {
             const allLinks = document.querySelectorAll('.icon')
