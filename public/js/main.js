a4a170f8855d6c4ce7e317f17bde8e7394efcb08
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-09-01 18:10:03 -0300

    listo practicamente las páginas de NoLog

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,10 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            page: 'contact',
+            page: 'home',
             about: '',
             game: '',
+            log: false,
         }
     },
     created() {
@@ -15,11 +16,10 @@ createApp({
         show() {
             document.querySelector('#open').classList.toggle('d-none')
             document.querySelector('#close').classList.toggle('d-none')
-            // document.querySelector('#aside').classList.toggle('hide')
             document.querySelector('#aside').classList.toggle('show')
-            // document.querySelector('#aside').classList.toggle('d-none')
             document.querySelector('#nav').classList.toggle('hide_nav')
             document.querySelector('#principal').classList.toggle('d-none')
+            if (this.page === 'rules') {document.querySelector('#back_top').classList.toggle('d-none')}
         },
         toggleClass: function(element) {
             const allLinks = document.querySelectorAll('.icon')
@@ -27,11 +27,36 @@ createApp({
             this.page = element
             document.querySelector(`#${element}`).classList.add('page')
         },
-        showAbout: function(element) {
-            this.about = `${element}`
+        showAbout: function(about, arrow) {
+            if (this.about === '') {
+                document.querySelector(`#${arrow}`).classList.add('rotate')
+                this.about = `${about}`
+            }
+            else if (this.about === about) {
+                this.about = ''
+                document.querySelector(`#${arrow}`).classList.remove('rotate')
+            }
+            else if (this.about !== about) {
+                this.about = `${about}`
+                document.querySelectorAll(`.arrow_class`)
+                    .forEach(e => e.classList.remove('rotate'))
+                document.querySelector(`#${arrow}`).classList.add('rotate')
+            }
         },
-        showGame: function(element) {
-            this.game = `${element}`
+        showGame: function(games) {
+            if (this.game === '') {
+                this.game = `${games}`
+            }
+            else if (this.game === games) {
+                this.game = ''
+            }
+            else if (this.game !== games) {
+                this.game = `${games}`
+            }
+        }, 
+        backTop: function() {
+            // document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
         }
     },
     computed: {
